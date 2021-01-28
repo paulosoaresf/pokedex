16ec552aa68bc5b118ccb364260434fb31981228
Paulo Soares <psoares@precima.com>
2021-01-28 11:06:25 -0500

    Proj Dev 2021 - Video 2 + Light and Dark Mode

---
+++ main.js
@@ -0,0 +1,28 @@
+const mode = document.querySelector('button.mode');
+const body = document.querySelector('body');
+
+const getCurrentMode = () => {
+  let currentMode = null;
+  if (body.classList.contains('light')) {
+    currentMode = 'light';
+  } else if (body.classList.contains('dark')) {
+    currentMode = 'dark';
+  }
+  return currentMode;
+};
+
+const toggleMainClass = () => {
+  const current = getCurrentMode();
+  body.classList.remove('light', 'dark');
+  if (current === 'light') {
+    body.classList.add('dark');
+  } else if (current === 'dark') {
+    body.classList.add('light');
+  }
+};
+
+const toggleMode = (e) => {
+  toggleMainClass();
+};
+
+mode.addEventListener('click', toggleMode);
